966ac0245d06c042d285d7249c17372e42035c2e
Gdarkstar <15co118.gautham@nitk.edu.in>
2018-03-16 15:24:21 +0530

    rnn cell created

--- seq2seq_model_for_Chatbot.py
+++ seq2seq_model_for_Chatbot.py
@@ -6,11 +6,11 @@ from __future__ import print_function
 
 import random
 
-import numpy as np
-from six.moves import xrange  # pylint: disable=redefined-builtin
-import tensorflow as tf
+import numpy as np #for sequence functions
+from six.moves import xrange  # pylint: disable=redefined-builtin  This function returns the generator object that can be used to display numbers only by looping
+import tensorflow as tf 
 
-from tensorflow.models.rnn.translate import data_utils
+from tensorflow.models.rnn.translate import data_utils #import translate model
 
 
 class Seq2SeqModel(object):
@@ -19,10 +19,10 @@ class Seq2SeqModel(object):
   def __init__(self, source_vocab_size, target_vocab_size, buckets, size,
                num_layers, max_gradient_norm, batch_size, learning_rate,
                learning_rate_decay_factor, use_lstm=False,
-               num_samples=512, forward_only=False):
-    self.source_vocab_size = source_vocab_size
+               num_samples=512, forward_only=False): #constructor for creating model
+    self.source_vocab_size = source_vocab_size 
     self.target_vocab_size = target_vocab_size
-    self.buckets = buckets
+    self.buckets = buckets     
     self.batch_size = batch_size
     self.learning_rate = tf.Variable(float(learning_rate), trainable=False)
     self.learning_rate_decay_op = self.learning_rate.assign(
@@ -39,4 +39,18 @@ class Seq2SeqModel(object):
       b = tf.get_variable("proj_b", [self.target_vocab_size])
       output_projection = (w, b)
 
+      def sampled_loss(inputs, labels):
+        labels = tf.reshape(labels, [-1, 1])
+        return tf.nn.sampled_softmax_loss(w_t, b, inputs, labels, num_samples,
+                self.target_vocab_size)
+      softmax_loss_function = sampled_loss
+
+    # Create the internal multi-layer cell for our RNN.
+    single_cell = tf.nn.rnn_cell.GRUCell(size)
+    if use_lstm:
+      single_cell = tf.nn.rnn_cell.BasicLSTMCell(size)
+    cell = single_cell
+    cell = tf.nn.rnn_cell.DropoutWrapper(cell, output_keep_prob=0.5)
+    if num_layers > 1:
+      cell = tf.nn.rnn_cell.MultiRNNCell([single_cell] * num_layers)
       
