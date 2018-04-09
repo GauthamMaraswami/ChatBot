e9181b384f4b3ed23a41a5929e2d96cb200d77a0
Gdarkstar <15co118.gautham@nitk.edu.in>
2018-04-09 14:45:50 +0530

    trainer added

---
+++ chatbot/trainner.py
@@ -0,0 +1,30 @@
+
+"""
+Train the program by launching it with random parametters
+"""
+
+from tqdm import tqdm
+import os
+
+
+def main():
+
+    dictParams = {
+        "batchSize": [int, [1, 3]]
+        "learningRate": [float, [1, 3]]
+        }
+
+    # Training multiple times with different parametters
+    for i in range(10):
+        # Generate the command line arguments
+        trainingArgs = ""
+        for keyArg, valueArg in dictParams:
+            value = str(random(valueArg[0], max=valueArg[1]))
+            trainingArgs += " --" + keyArg + " " + value
+
+        # Launch the program
+        os.run("main.py" + trainingArgs)
+
+
+if __name__ == "__main__":
+    main()
