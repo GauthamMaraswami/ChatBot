4a5799a03f1b0d6827a409a47fd422b794658004
Gdarkstar <15co118.gautham@nitk.edu.in>
2018-04-13 14:45:50 +0530

    files removed and cleaned

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -1,57 +1,28 @@
-
-"""
-Main script. See README.md for more information
-
-Use python 3
-"""
-
-import argparse  # Command line parsing
-import configparser  # Saving the models parameters
-import datetime  # Chronometer
-import os  # Files management
+import argparse  
+import configparser  
+import datetime  
+import os  
 import tensorflow as tf
 import numpy as np
 import math
-
-from tqdm import tqdm  # Progress bar
+from tqdm import tqdm  
 from tensorflow.python import debug as tf_debug
-
 from chatbot.textdata import TextData
 from chatbot.model import Model
-
-
 class Chatbot:
-    """
-    Main class which launch the training or testing mode
-    """
-
     class TestMode:
-        """ Simple structure representing the different testing modes
-        """
         ALL = 'all'
-        INTERACTIVE = 'interactive'  # The user can write his own questions
-        DAEMON = 'daemon'  # The chatbot runs on background and can regularly be called to predict something
+        INTERACTIVE = 'interactive' 
 
     def __init__(self):
-        """
-        """
-        # Model/dataset parameters
         self.args = None
-
-        # Task specific object
-        self.textData = None  # Dataset
-        self.model = None  # Sequence to sequence model
-
-        # Tensorflow utilities for convenience saving/logging
+        self.textData = None  
+        self.model = None  
         self.writer = None
         self.saver = None
-        self.modelDir = ''  # Where the model is saved
-        self.globStep = 0  # Represent the number of iteration for the current model
-
-        # TensorFlow main session (we keep track for the daemon)
+        self.modelDir = ''  
+        self.globStep = 0 
         self.sess = None
-
-        # Filename and directories constants
         self.MODEL_DIR_BASE = 'save' + os.sep + 'model'
         self.MODEL_NAME_BASE = 'model'
         self.MODEL_EXT = '.ckpt'
@@ -63,95 +34,39 @@ class Chatbot:
 
     @staticmethod
     def parseArgs(args):
-
-
         parser = argparse.ArgumentParser()
-
-        # Global options
         globalArgs = parser.add_argument_group('Global options')
         globalArgs.add_argument('--test',
                                 nargs='?',
                                 choices=[Chatbot.TestMode.ALL, Chatbot.TestMode.INTERACTIVE, Chatbot.TestMode.DAEMON],
                                 const=Chatbot.TestMode.ALL, default=None,
-                                help='if present, launch the program try to answer all sentences from data/test/ with'
-                                     ' the defined model(s), in interactive mode, the user can wrote his own sentences,'
-                                     ' use daemon mode to integrate the chatbot in another program')
-        globalArgs.add_argument('--createDataset', action='store_true', help='if present, the program will only generate the dataset from the corpus (no training/testing)')
-        globalArgs.add_argument('--playDataset', type=int, nargs='?', const=10, default=None,  help='if set, the program  will randomly play some samples(can be use conjointly with createDataset if this is the only action you want to perform)')
-        globalArgs.add_argument('--reset', action='store_true', help='use this if you want to ignore the previous model present on the model directory (Warning: the model will be destroyed with all the folder content)')
-        globalArgs.add_argument('--verbose', action='store_true', help='When testing, will plot the outputs at the same time they are computed')
-        globalArgs.add_argument('--debug', action='store_true', help='run DeepQA with Tensorflow debug mode. Read TF documentation for more details on this.')
-        globalArgs.add_argument('--keepAll', action='store_true', help='If this option is set, all saved model will be kept (Warning: make sure you have enough free disk space or increase saveEvery)')  # TODO: Add an option to delimit the max size
-        globalArgs.add_argument('--modelTag', type=str, default=None, help='tag to differentiate which model to store/load')
-        globalArgs.add_argument('--rootDir', type=str, default=None, help='folder where to look for the models and data')
-        globalArgs.add_argument('--watsonMode', action='store_true', help='Inverse the questions and answer when training (the network try to guess the question)')
-        globalArgs.add_argument('--autoEncode', action='store_true', help='Randomly pick the question or the answer and use it both as input and output')
-        globalArgs.add_argument('--device', type=str, default=None, help='\'gpu\' or \'cpu\' (Warning: make sure you have enough free RAM), allow to choose on which hardware run the model')
-        globalArgs.add_argument('--seed', type=int, default=None, help='random seed for replication')
+                                help='if ')
+        
 
-        # Dataset options
-        datasetArgs = parser.add_argument_group('Dataset options')
-        datasetArgs.add_argument('--corpus', choices=TextData.corpusChoices(), default=TextData.corpusChoices()[0], help='corpus on which extract the dataset.')
-        datasetArgs.add_argument('--datasetTag', type=str, default='', help='add a tag to the dataset (file where to load the vocabulary and the precomputed samples, not the original corpus). Useful to manage multiple versions. Also used to define the file used for the lightweight format.')  # The samples are computed from the corpus if it does not exist already. There are saved in \'data/samples/\'
-        datasetArgs.add_argument('--ratioDataset', type=float, default=1.0, help='ratio of dataset used to avoid using the whole dataset')  # Not implemented, useless ?
-        datasetArgs.add_argument('--maxLength', type=int, default=10, help='maximum length of the sentence (for input and output), define number of maximum step of the RNN')
-        datasetArgs.add_argument('--filterVocab', type=int, default=1, help='remove rarelly used words (by default words used only once). 0 to keep all words.')
-        datasetArgs.add_argument('--skipLines', action='store_true', help='Generate training samples by only using even conversation lines as questions (and odd lines as answer). Useful to train the network on a particular person.')
-        datasetArgs.add_argument('--vocabularySize', type=int, default=40000, help='Limit the number of words in the vocabulary (0 for unlimited)')
-
-        # Network options (Warning: if modifying something here, also make the change on save/loadParams() )
         nnArgs = parser.add_argument_group('Network options', 'architecture related option')
-        nnArgs.add_argument('--hiddenSize', type=int, default=512, help='number of hidden units in each RNN cell')
-        nnArgs.add_argument('--numLayers', type=int, default=2, help='number of rnn layers')
-        nnArgs.add_argument('--softmaxSamples', type=int, default=0, help='Number of samples in the sampled softmax loss function. A value of 0 deactivates sampled softmax')
-        nnArgs.add_argument('--initEmbeddings', action='store_true', help='if present, the program will initialize the embeddings with pre-trained word2vec vectors')
-        nnArgs.add_argument('--embeddingSize', type=int, default=64, help='embedding size of the word representation')
-        nnArgs.add_argument('--embeddingSource', type=str, default="GoogleNews-vectors-negative300.bin", help='embedding file to use for the word representation')
-
-        # Training options
         trainingArgs = parser.add_argument_group('Training options')
-        trainingArgs.add_argument('--numEpochs', type=int, default=30, help='maximum number of epochs to run')
-        trainingArgs.add_argument('--saveEvery', type=int, default=2000, help='nb of mini-batch step before creating a model checkpoint')
-        trainingArgs.add_argument('--batchSize', type=int, default=256, help='mini-batch size')
-        trainingArgs.add_argument('--learningRate', type=float, default=0.002, help='Learning rate')
-        trainingArgs.add_argument('--dropout', type=float, default=0.9, help='Dropout rate (keep probabilities)')
-
         return parser.parse_args(args)
 
     def main(self, args=None):
-        """
-        Launch the training and/or the interactive mode
-        """
-        print('Welcome to DeepQA v0.1 !')
-        print()
         print('TensorFlow detected: v{}'.format(tf.__version__))
-
-
         self.args = self.parseArgs(args)
-
         if not self.args.rootDir:
-            self.args.rootDir = os.getcwd()  # Use the current working directory
-
+            self.args.rootDir = os.getcwd()  
         self.loadModelParams()  
-
         self.textData = TextData(self.args)
-
         if self.args.createDataset:
-            print('Dataset created! Thanks for using this program')
-            return  # No need to go further
+            return  
 
-        # Prepare the model
         with tf.device(self.getDevice()):
             self.model = Model(self.args, self.textData)
 
-        # Saver/summaries
         self.writer = tf.summary.FileWriter(self._getSummaryName())
         self.saver = tf.train.Saver(max_to_keep=200)
 
      
         self.sess = tf.Session(config=tf.ConfigProto(
-            allow_soft_placement=True,  # Allows backup device for non GPU-available operations (when forcing GPU)
-            log_device_placement=False)  # Too verbose ?
+            allow_soft_placement=True, 
+            log_device_placement=False)  
         ) 
 
         if self.args.debug:
@@ -164,7 +79,6 @@ class Chatbot:
         if self.args.test != Chatbot.TestMode.ALL:
             self.managePreviousModel(self.sess)
 
-        # Initialize embeddings with pre-trained word2vec vectors
         if self.args.initEmbeddings:
             self.loadEmbedding(self.sess)
 
@@ -178,7 +92,7 @@ class Chatbot:
             elif self.args.test == Chatbot.TestMode.DAEMON:
                 print('Daemon mode, running in background...')
             else:
-                raise RuntimeError('Unknown test mode: {}'.format(self.args.test))  # Should never happen
+                raise RuntimeError('Unknown test mode: {}'.format(self.args.test)) 
         else:
             self.mainTrain(self.sess)
 
@@ -188,7 +102,7 @@ class Chatbot:
 
     def mainTrain(self, sess):
 
-        self.textData.makeLighter(self.args.ratioDataset)  # Limit the number of training samples
+        self.textData.makeLighter(self.args.ratioDataset) 
 
         mergedSummaries = tf.summary.merge_all()  
         if self.globStep == 0:  
@@ -207,29 +121,26 @@ class Chatbot:
 
                 tic = datetime.datetime.now()
                 for nextBatch in tqdm(batches, desc="Training"):
-                    # Training pass
                     ops, feedDict = self.model.step(nextBatch)
-                    assert len(ops) == 2  # training, loss
+                    assert len(ops) == 2  
                     _, loss, summary = sess.run(ops + (mergedSummaries,), feedDict)
                     self.writer.add_summary(summary, self.globStep)
                     self.globStep += 1
 
-                    # Output training status
                     if self.globStep % 100 == 0:
                         perplexity = math.exp(float(loss)) if loss < 300 else float("inf")
                         tqdm.write("----- Step %d -- Loss %.2f -- Perplexity %.2f" % (self.globStep, loss, perplexity))
 
-                    # Checkpoint
                     if self.globStep % self.args.saveEvery == 0:
                         self._saveSession(sess)
 
                 toc = datetime.datetime.now()
 
                 print("Epoch finished in {}".format(toc-tic))  
-        except (KeyboardInterrupt, SystemExit):  # If the user press Ctrl+C while testing progress
+        except (KeyboardInterrupt, SystemExit):  
             print('Interruption detected, exiting the program...')
 
-        self._saveSession(sess)  # Ultimate saving before complete exit
+        self._saveSession(sess) 
 
     def predictTestset(self, sess):
 
@@ -265,21 +176,17 @@ class Chatbot:
 
     def mainTestInteractive(self, sess):
 
-        print('Testing: Launch interactive mode:')
-        print('')
-        print('Welcome to the interactive mode, here you can ask to Deep Q&A the sentence you want. Don\'t have high '
-              'expectation. Type \'exit\' or just press ENTER to quit the program. Have fun.')
-
+        
         while True:
             question = input(self.SENTENCES_PREFIX[0])
             if question == '' or question == 'exit':
                 break
 
-            questionSeq = []  # Will be contain the question as seen by the encoder
+            questionSeq = []  
             answer = self.singlePredict(question, questionSeq)
             if not answer:
                 print('Warning: sentence too long, sorry. Maybe try a simpler sentence.')
-                continue  # Back to the beginning, try again
+                continue 
 
             print('{}{}'.format(self.SENTENCES_PREFIX[1], self.textData.sequence2str(answer, clean=True)))
 
@@ -380,8 +287,7 @@ class Chatbot:
         if os.listdir(self.modelDir):
             if self.args.reset:
                 print('Reset: Destroying previous model at {}'.format(self.modelDir))
-            # Analysing directory content
-            elif os.path.exists(modelName):  # Restore the model
+            elif os.path.exists(modelName): 
                 print('Restoring previous model from {}'.format(modelName))
                 self.saver.restore(sess, modelName)  
             elif self._getModelList():
@@ -396,7 +302,6 @@ class Chatbot:
                 for f in fileList:
                     print('Removing {}'.format(f))
                     os.remove(f)
-
         else:
             print('No previous model found, starting from clean directory: {}'.format(self.modelDir))
 
@@ -513,7 +418,7 @@ class Chatbot:
     def _getModelName(self):
 
         modelName = os.path.join(self.modelDir, self.MODEL_NAME_BASE)
-        if self.args.keepAll:  # We do not erase the previously saved model by including the current step on the name
+        if self.args.keepAll:
             modelName += '-' + str(self.globStep)
         return modelName + self.MODEL_EXT
 
@@ -523,7 +428,7 @@ class Chatbot:
             return '/cpu:0'
         elif self.args.device == 'gpu':
             return '/gpu:0'
-        elif self.args.device is None:  # No specified device (default)
+        elif self.args.device is None:  
             return None
         else:
             print('Warning: Error in the device name: {}, use the default device'.format(self.args.device))
